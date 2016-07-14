30ccc97ff11f1ba4a4972b331808668d1da81c81
Oleksii Gulchenko <oleksii.gulchenko@globallogic.com.ua>
2016-07-14 19:53:53 +0300

    Android.mk: Added rules for build tests.
    Change-Id: I836a59693fa668dfbe4e1dac368770027db12828
    Signed-off-by: Oleksii Gulchenko <oleksii.gulchenko@globallogic.com.ua>

--- Android.mk
+++ Android.mk
@@ -16,3 +16,31 @@ LOCAL_MODULE:= libvspmif
 LOCAL_SHARED_LIBRARIES := libmmngr
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES:= \
+    $(LOCAL_PATH)/vspm_if-module/files/vspm_if/include \
+    hardware/renesas/mmngr/libmmngr/mmngr/include
+
+LOCAL_MODULE := vspm_tp
+LOCAL_SHARED_LIBRARIES := libmmngr libvspmif
+
+LOCAL_SRC_FILES := \
+    vspm_if-tp-user/files/vspm_if/vspm_tp_lossy.c
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES:= \
+    $(LOCAL_PATH)/vspm_if-module/files/vspm_if/include \
+    hardware/renesas/mmngr/libmmngr/mmngr/include
+
+LOCAL_MODULE := fdpm_tp
+LOCAL_SHARED_LIBRARIES := libmmngr libvspmif
+
+LOCAL_SRC_FILES := \
+    vspm_if-tp-user/files/vspm_if/fdpm_tp.c
+
+include $(BUILD_EXECUTABLE)
